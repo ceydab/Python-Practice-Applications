3a26653e35a05d9e6d4686497824ff2c495a3d05
ceydab <74830373+ceydab@users.noreply.github.com>
2021-07-29 20:14:10 +0300

    Use BeautifulSoup to Get Information

---
+++ app8.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from bs4.dammit import html_meta
+import requests
+
+url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+
+html = requests.get(url).content
+
+soup = BeautifulSoup(html, "html.parser")
+count = 1
+listmovies = soup.find("tbody", {"class":"lister-list"}).find_all("tr", limit=50)
+for tr in listmovies:
+    title = tr.find("td", {"class":"titleColumn"}).find("a").text
+    year = tr.find("td", {"class":"titleColumn"}).find("span").text
+    rating = tr.find("td", {"class":"ratingColumn"}).find("strong").text
+    print(str(count) + ". " + title.ljust(50) + " "+ year + " = " + rating )
+    count += 1
