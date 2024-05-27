b72a9893d946da53a1aefa0ae4adf9727e778cdf
ceydab <74830373+ceydab@users.noreply.github.com>
2024-05-28 00:05:36 +0200

    add comments

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,4 +1,7 @@
-#find prime numbers
+'''this project is a simple application of python for listing prime numbers. the projects gets a number as an input
+and through a while loop, it calculated modulus of each number up until the input number, and lists the numbers as 
+prime numbers if they have modulus 0 for as least one value other than zero and themselves. 
+'''
 
 i = 0
 number = int(input('give a number: '))
